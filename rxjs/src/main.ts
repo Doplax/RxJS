0c386a43a30bd5244c96a7f96fbcb468e1e4e2eb
Doplax <doplax@gmail.com>
2023-11-17 10:50:30 +0100

    Final clase 17 - Subscription y Unsubscribe

--- rxjs/src/main.ts
+++ rxjs/src/main.ts
@@ -1,2 +1,45 @@
 import './style.css'
-import './Ejercicios/creacion-observable'
+import { Observable, Observer, Subscriber, count, of } from "rxjs";
+
+//import './Observables/01-observables'
+
+// Interfaz
+const observer: Observer<any> = {
+  next: value => console.log('Next:',value),
+  error: error => console.warn('Error:',error),
+  complete: () => console.info('completado')
+
+}
+
+const intervalo$ = new Observable<number>(Subscriber => {
+  
+  // Crear un contador, 1,2,3,4,5...
+  let contador:number  = 0;
+
+  const interval = setInterval(() => {
+    // Cada Segundo
+    ++contador;
+    Subscriber.next( contador );
+    console.log(contador);
+  },1000);
+
+  return () => {
+    clearInterval(interval)
+    console.log('Intervalo Destruido');
+  }
+})
+
+const subs1 = intervalo$.subscribe();
+const subs2 = intervalo$.subscribe();
+const subs3 = intervalo$.subscribe();
+
+// Cancelariamos la subscription despues del timepo indicado
+setTimeout(() => { 
+
+
+  subs1.unsubscribe() 
+  subs2.unsubscribe() 
+  subs3.unsubscribe() 
+
+  console.log('Completado timeout');
+},4000)
